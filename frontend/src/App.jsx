7c12239de1e3821c081d78e94a32bb7c17d2d447
kaushal <kaushalkishorroy21@gmail.com>
2024-04-21 17:25:44 +0530

    .new

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,20 @@
 // import { useState } from 'react';
 import { Navbar } from '@material-tailwind/react';
 import './App.css';
-import LoginPage from './pages/LoginPage';
+// import LoginPage from './pages/LoginPage';
 
 // import {Button} from '@material-tailwind/react'
 import Test from './Test';
+import Register from './pages/RegisterPage';
 function App() {
   // const [count, setCount] = useState(0);
 
   return (
     <>
-    <LoginPage/>
+    {/* <LoginPage/> */}
 <Navbar/>
       <Test/>
+      <Register/>
     </>
   )
 }
